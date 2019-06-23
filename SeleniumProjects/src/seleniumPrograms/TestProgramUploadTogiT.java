8f8f9960cd64f28d800b27ce225774fb36991913
ANAND <ANAND@DESKTOP-IKH2BOV>
2019-06-24 00:32:42 +0530

    Added New Program

---
+++ SeleniumProjects/src/seleniumPrograms/TestProgramUploadTogiT.java
@@ -0,0 +1,12 @@
+package seleniumPrograms;
+
+import org.testng.annotations.Test;
+
+public class TestProgramUploadTogiT {
+	@Test
+	public void testuploadtoGit() {
+		
+		System.out.println("This is Testing program upload to Git");
+	}
+
+}
