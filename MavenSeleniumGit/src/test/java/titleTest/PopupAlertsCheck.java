ed83ffd40fa1096855e43dee508e69d06ab8d141
ANAND <babu.nalluri7@gmail.com>
2019-06-23 07:20:38 +0530

    Added New class

---
+++ MavenSeleniumGit/src/test/java/titleTest/PopupAlertsCheck.java
@@ -0,0 +1,14 @@
+package titleTest;
+
+import org.testng.annotations.Test;
+
+public class PopupAlertsCheck
+{
+@Test
+
+
+public void popupcheck() {
+	System.out.println("This is Popup check");
+}
+
+}
